bc980f5ad645fa43a1ced23275d4e484925db433
rk308 <rk308@ITL049.student.eecs.qmul.ac.uk>
2017-10-13 20:25:23 +0100

    Tabs

--- BarberProgram/src/Main.java
+++ BarberProgram/src/Main.java
@@ -1,9 +1,53 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.geometry.Side;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Testing.");
-	}
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Tabs");
+        Group root = new Group();
+        Scene scene = new Scene(root, 400, 250, Color.WHITE);
+
+        TabPane tabPane = new TabPane();
+
+        BorderPane borderPane = new BorderPane();
+        for (int i = 0; i < 5; i++) {
+            Tab tab = new Tab();
+            tab.setText("Tab" + i);
+            HBox hbox = new HBox();
+            hbox.getChildren().add(new Label("Tab" + i));
+            hbox.setAlignment(Pos.CENTER);
+            tab.setContent(hbox);
+            tabPane.getTabs().add(tab);
+        }
+        // bind to take available space
+        borderPane.prefHeightProperty().bind(scene.heightProperty());
+        borderPane.prefWidthProperty().bind(scene.widthProperty());
+        
+        borderPane.setCenter(tabPane);
+        root.getChildren().add(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
 }
