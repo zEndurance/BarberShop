b10b7d16cfd0c0a659b63de591773952ea9e1ec0
rk16449 <32778886+rk16449@users.noreply.github.com>
2017-10-17 12:35:47 +0100

    creating a structure for each data in the table

--- BarberProgram/src/controllersPackage/AccountTabController.java
+++ BarberProgram/src/controllersPackage/AccountTabController.java
@@ -3,13 +3,20 @@ package controllersPackage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TreeTableView;
 
 public class AccountTabController implements Initializable {
+	
+	
+
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		
+
 	}
 }
 
--- BarberProgram/src/controllersPackage/AppointTabController.java
+++ BarberProgram/src/controllersPackage/AppointTabController.java
@@ -3,13 +3,16 @@ package controllersPackage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
 public class AppointTabController implements Initializable {
 
+	@FXML TableView appointTable;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		
 	}
 
