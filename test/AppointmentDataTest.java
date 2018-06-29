d44acf0aa4a7a916f97b5e0d44e4e450e3faa0c1
Raj <32778886+rk16449@users.noreply.github.com>
2018-06-29 23:07:55 +0100

    restructured tests

---
+++ test/AppointmentDataTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppointmentDataTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
