6006cf0df874db1b61e2b74979a27364e75168de
Raj <32778886+rk16449@users.noreply.github.com>
2018-07-06 23:08:41 +0100

    cleaned up main controller and removed useless code

--- src/controllersPackage/MainProgramController.java
+++ src/controllersPackage/MainProgramController.java
@@ -1,17 +1,10 @@
 package controllersPackage;
 
-import java.io.BufferedReader;
 /* Import java, javafx */
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -23,29 +16,26 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import mainPackage.Connection;
 import mainPackage.User;
  
 public class MainProgramController implements Initializable {
 	
     @FXML
 	protected void handleSubmitButtonAction(ActionEvent event) throws IOException {
+    	
     	// Logout Alert Window
     	Alert alert = new Alert(AlertType.NONE);
     	alert.setContentText("Are you sure you want to logout?");
     	ButtonType buttonTypeOne = new ButtonType("Yes");
     	ButtonType buttonTypeTwo = new ButtonType("No");
-    	
     	alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo);
-  
     	Optional<ButtonType> result = alert.showAndWait();
+    	
+    	// Logout
     	if (result.get() == buttonTypeOne){
-    		
     		// Reset the users data
     		User.getInstance().logout();
-    		
     		Parent blah = FXMLLoader.load(getClass().getResource("/fxmlPackage/loginPage.fxml"));
     		Scene scene = new Scene(blah);
     		Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -54,12 +44,11 @@ public class MainProgramController implements Initializable {
     		appStage.setWidth(944);
     		appStage.setHeight(600);
     		appStage.show();
-    	}else if(result.get() == buttonTypeOne) {
     	}
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO - MySQL grabbing all user data should happen here and not each time a tab opens
+		
 	}
 }
